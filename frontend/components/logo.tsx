9e90ef32026c6fbdee7376979f2c7dab1bfaec6f
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-10-09 09:54:06 -0500

    Update logo.tsx

--- frontend/components/logo.tsx
+++ frontend/components/logo.tsx
@@ -8,12 +8,12 @@ export const Logo = () => {
       href="/"
       className="font-normal flex space-x-2 items-center text-sm mr-4  text-black px-2 py-1  relative z-20"
     >
-      <Image
+      {/* <Image
         src="https://assets.aceternity.com/logo-dark.png"
         alt="logo"
         width={30}
         height={30}
-      />
+      /> */}
       <span className="font-medium text-black dark:text-white">MDL-13</span>
     </Link>
   );
