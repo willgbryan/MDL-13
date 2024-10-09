385fee96c20277a428af9758d70f50d7300cab9d
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-10-08 22:14:37 -0500

    formatting

--- frontend/components/picks.tsx
+++ frontend/components/picks.tsx
@@ -659,12 +659,12 @@ export default function Picks() {
             </TableBody>
             </Table>
         </CardContent>
-        <CardFooter>
+        {/* <CardFooter>
             <div className="text-xs text-muted-foreground">
             Showing <strong>1-10</strong> of <strong>32</strong>{" "}
             products
             </div>
-        </CardFooter>
+        </CardFooter> */}
         </Card>
       </div>
     </div>
