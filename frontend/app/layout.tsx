59599540e5bd4cd455a9c94b717a34c80502b1b9
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-10-10 14:56:15 -0500

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,13 +5,13 @@ import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { ThemeProvider } from "@/context/providers";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "MDL-13",
-  description:
-    "Applied machine learning for sports analytics.",
+  description: "Applied machine learning for sports analytics.",
 };
 
 export default function RootLayout({
@@ -21,8 +21,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+      </Head>
       <body
-        className={cn("antialiased dark:bg-black bg-white", inter.className)}
+        className={cn(
+          "antialiased dark:bg-black bg-white",
+          inter.className,
+          "min-h-screen",
+          "p-safe"
+        )}
       >
         <ThemeProvider
           attribute="class"
@@ -30,9 +38,11 @@ export default function RootLayout({
           enableSystem={false}
           disableTransitionOnChange
         >
-          <Navbar />
-          {children}
-          <Footer />
+          <div className="flex flex-col min-h-screen">
+            <Navbar className="sticky top-0 pt-safe-top px-safe" />
+            <main className="flex-grow px-safe">{children}</main>
+            <Footer className="px-safe pb-safe" />
+          </div>
         </ThemeProvider>
       </body>
     </html>
