25fbb04505e51d727bc11e84aa748b2108aca120
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-10-14 15:13:21 -0500

    actually good to go

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -5,6 +5,6 @@ import Stripe from 'stripe'
 const stripeSecretKey = process.env.STRIPE_SECRET_KEY ?? process.env.STRIPE_SECRET_KEY ?? ''
 
 export const stripe = new Stripe(stripeSecretKey, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2024-09-30.acacia',
   typescript: true,
 })
