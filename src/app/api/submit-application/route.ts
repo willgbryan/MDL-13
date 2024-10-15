cf124b0f3385d4480b9e112551a8c5c9c05ff713
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-10-14 20:13:41 -0500

    guess we're doing this

--- src/app/api/submit-application/route.ts
+++ src/app/api/submit-application/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
 export async function POST(req: NextRequest) {
