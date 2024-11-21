d736002d1f8f8f81718ab652b365dfcaf0489ee9
willgbryan <107731540+willgbryan@users.noreply.github.com>
2024-11-21 13:13:02 -0600

    Update week-12.tsx

--- src/app/picks/week-12/week-12.tsx
+++ src/app/picks/week-12/week-12.tsx
@@ -183,7 +183,7 @@ export default function Picks() {
         <div className="flex min-h-screen w-full flex-col items-center justify-center py-20 bg-transparent">
           <Header>
             <h2 className="font-sans text-bold text-xl text-center md:text-4xl w-fit mx-auto font-bold tracking-tight text-neutral-800 dark:text-neutral-100">
-              Week 11 Predictions
+              Week 12 Predictions
             </h2>
           </Header>
           <div className="w-full max-w-7xl px-4 py-8">
@@ -191,7 +191,7 @@ export default function Picks() {
               <CardHeader>
                 <CardTitle>MDL Predictions</CardTitle>
                 <CardDescription>
-                  MDL-13 predictions for Week 11 NFL games. These picks are based on historical data, use your own best judgement if bets are placed.
+                  MDL-13 predictions for Week 12 NFL games. These picks are based on historical data, use your own best judgement if bets are placed.
                 </CardDescription>
               </CardHeader>
               <CardContent className="overflow-x-auto">
